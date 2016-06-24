f9045b9f3c7f5f66602697d490c0f4d329917355
Mo Amin <m.alamin92@gmail.com>
2016-06-24 01:27:55 -0400

    spelling error on grade view controller fixed

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -1,5 +1,5 @@
 class GradesController < ApplicationController
-  before_action :authenticat_user!
+  before_action :authenticate_user!
   before_action :set_grade, only: [:show, :edit, :update, :destroy]
 
   # GET /grades
